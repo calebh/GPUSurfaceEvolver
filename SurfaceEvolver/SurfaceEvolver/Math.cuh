2cf09ea2d0101796ea4ab7be1626d138e13af794
Caleb Helbling <caleb.helbling@gmail.com>
2015-05-02 21:09:39 -0400

    Removed old surface evolver

--- SurfaceEvolver/SurfaceEvolver/Math.cuh
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "cuda.h"
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-#include "CudaGeom.h"
-
-__device__ vector subtractPoints(point a, point b);
-__device__ vector crossProduct(vector u, vector v);
