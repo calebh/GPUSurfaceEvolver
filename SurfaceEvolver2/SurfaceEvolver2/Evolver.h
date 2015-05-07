52ab2614817220b21883f0209e8c7abb75e9286c
digitaltembo <thatwhich@dijitalelefan.com>
2015-05-07 16:58:30 -0400

    Evolver

--- SurfaceEvolver2/SurfaceEvolver2/Evolver.h
+++ SurfaceEvolver2/SurfaceEvolver2/Evolver.h
@@ -2,7 +2,9 @@
 
 #pragma once
 
-enum OutputType { TOTAL_SURFACE_AREA, TOTAL_VOLUME, MEAN_NET_FORCE, MEAN_CURVATURE, POINTS, FORCES, NONE };
+enum OutputType { TOTAL_SURFACE_AREA, TOTAL_VOLUME, MEAN_NET_FORCE,
+                  MEAN_CURVATURE, POINTS, AREA_FORCES, VOLUME_FORCES,
+                  NET_FORCES };
 
 class Evolver
 {
@@ -12,7 +14,7 @@ public:
 	float findLambda();
 	void update();
         
-        
+        void setOutputFormat(OutputType* format, int formatLength );
 	void outputData();
         
 private:
@@ -21,6 +23,12 @@ private:
 	virtual float getMeanNetForce() = 0;
 	virtual float getMeanCurvature() = 0;
 	virtual float getVolume() = 0;
+        
+        virtual void outputPoints() = 0;
+        virtual void outputVolumeForces() = 0;
+        virtual void outputAreaForces() = 0;
+        virtual void outputNetForces() = 0;
+        
 	Mesh* mesh;
 	float lambda;
 	int itersUntilLambdaUpdate;
